0f099e5074ebb87a46665b5ee7f2850e0c782f7e
avihu.hababo <avihulink@gmail.com>
2024-05-13 14:43:14 +0300

    check

--- hello/obj/Release/net8.0/hello.AssemblyInfo.cs
+++ hello/obj/Release/net8.0/hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b4b0dabce86ef35d0819860ef4e657f6a7ea78")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
